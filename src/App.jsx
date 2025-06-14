0315b721adc05c95fc8ce48b3506d52d067c54ff
Nana Aba Ackah <79535653+nanaabaackah@users.noreply.github.com>
2025-06-14 11:43:24 -0400

    resume update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-10 text-3xl font-bold">
+      Tailwind is working!
+    </div>
+  );
+}
+
+
+export default App;
+
